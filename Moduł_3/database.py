735743ce9fa1847aa62da25d952853262ce7b4e1
Czaja <dczajka1@gmail.com>
2023-11-08 10:39:48 +0100

    created new class DataBaseController

--- Moduł_3/database.py
+++ Moduł_3/database.py
@@ -1,16 +1,7 @@
 from sys import argv
 from sqlalchemy import create_engine, Column, Integer, String, select, Date, delete
-# from sqlalchemy.sql import func
 from sqlalchemy.orm import sessionmaker, declarative_base
 from datetime import datetime, date
-
-# tożsame:
-# with sqlite3.connect('books.db') as connection:
-# cursor = connection.cursor()
-# cursor.execute()
-# connection.commit()
-
-engine = create_engine('sqlite:///lent_books.db')
 
 Base = declarative_base()  # model klasy obieków
 
@@ -26,80 +17,93 @@ class LentBook(Base):
     return_at = Column(Date, nullable=False)
 
 
-def create_db():
-    Base.metadata.create_all(engine)
+class DataBaseController:
 
+    def __init__(self):
+        self.engine = create_engine('sqlite:///lent_books.db')
 
-Session = sessionmaker(bind=engine)
+    def create_connection(self):
+        Session = sessionmaker(bind=self.engine)
 
+        return Session()
 
-def add_book(email_address: str, name_who_lent_book: str, borrowed_book_title: str, to_be_return_at: str):
-    session = Session()
-    return_date = datetime.strptime(to_be_return_at, "%Y-%m-%d").date()
-    new_lent_book = LentBook(
-        email=email_address,
-        name=name_who_lent_book,
-        book_title=borrowed_book_title,
-        return_at=return_date
-    )
-    session.add(new_lent_book)
-    session.commit()
+    def create_db(self):
+        Base.metadata.create_all(self.engine)
 
+    def add_book(self, email_address: str, name_who_lent_book: str, borrowed_book_title: str, to_be_return_at: str):
+        session = self.create_connection()
 
-def show_all():
-    session = Session()
-    # Przykład pobrania wszystkich rekordów
-    all_lent_books = session.query(LentBook).all()
-    for lent_book in all_lent_books:
-        print(f"""
-            ID: {lent_book.id}, 
-            Email: {lent_book.email}, 
-            Name: {lent_book.name}, 
-            Book Title: {lent_book.book_title}, 
-            Lent At: {lent_book.lent_at.date()}
-            Return At: {lent_book.return_at.date()}""")
+        return_date = datetime.strptime(to_be_return_at, "%Y-%m-%d").date()
+        new_lent_book = LentBook(
+            email=email_address,
+            name=name_who_lent_book,
+            book_title=borrowed_book_title,
+            return_at=return_date
+        )
+        session.add(new_lent_book)
+        session.commit()
 
+    def get_all(self):
+        session = self.create_connection()
 
-def date_check():
-    today = datetime.today().date()
-    session = Session()
+        all_lent_books = session.query(LentBook).all()
+        return all_lent_books
 
-    # Przykład pobrania wszystkich rekordów
+    def date_check(self):
+        today = datetime.today().date()
+        session = self.create_connection()
 
-    books_to_return = session.execute(
-        select(LentBook).where(LentBook.return_at <= today))
+        books_to_return = session.execute(
+            select(LentBook).where(LentBook.return_at <= today))
 
-    for lent_book in books_to_return.scalars():
+        for lent_book in books_to_return.scalars():
 
-        print(f"""
-                Who lent book: {lent_book.name}
-                Email: {lent_book.email},
-                Book Title: {lent_book.book_title},
-                Return At: {lent_book.return_at.date()}""")
+            print(f"""
+                    Who lent book: {lent_book.name}
+                    Email: {lent_book.email},
+                    Book Title: {lent_book.book_title},
+                    Return At: {lent_book.return_at}""")
 
+    def clear_db(self):
+        session = self.create_connection()
 
-def clear_db():
-    session = Session()
-    for book in session.execute(select(LentBook)).all():
-        stmt = delete(book)
-        session.execute(stmt)
-    session.commit()
+        for book in session.execute(select(LentBook)).all():
+            stmt = delete(book)
+            session.execute(stmt)
+        session.commit()
 
 
 if __name__ == '__main__':
     if len(argv) == 2 and argv[1] == 'create-db':
-        create_db()
+        dbc = DataBaseController()
+        dbc.create_db()
+
     elif len(argv) == 2 and argv[1] == 'add-book':
+        dbc = DataBaseController()
         email = input('email: ')
         name = input('name: ')
         book_title = input('book_title: ')
         return_at = input('return_at [yyyy-mm-dd]: ')
-        add_book(email, name, book_title, return_at)
+        dbc.add_book(email, name, book_title, return_at)
+
     elif len(argv) == 2 and argv[1] == 'show-books':
-        show_all()
+        dbc = DataBaseController()
+        all_lent_books = dbc.get_all()
+        for lent_book in all_lent_books:
+            print(f"""
+                ID: {lent_book.id}, 
+                Email: {lent_book.email}, 
+                Name: {lent_book.name}, 
+                Book Title: {lent_book.book_title}, 
+                Lent At: {lent_book.lent_at}
+                Return At: {lent_book.return_at}""")
+
     elif len(argv) == 2 and argv[1] == 'clear':
-        clear_db()
+        dbc = DataBaseController()
+        dbc.clear_db()
+
     else:
-        date_check()
+        dbc = DataBaseController()
+        dbc.date_check()
 
 
