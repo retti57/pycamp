a090be3d07a46a08c1b6ad8cd515d2001817c33c
Czaja <dczajka1@gmail.com>
2023-11-13 22:31:37 +0100

    2023-11-13 added to db update by date of lent or return funciton

---
+++ Moduł_3/main.py
@@ -0,0 +1,49 @@
+from datetime import datetime
+from sys import argv
+from database import DataBaseController
+
+
+if __name__ == '__main__':
+    if len(argv) == 2 and argv[1] == 'create-db':
+        dbc = DataBaseController()
+        dbc.create_db()
+
+    elif len(argv) == 2 and argv[1] == 'add-book':
+        dbc = DataBaseController()
+        email = input('email: ')
+        name = input('name: ')
+        book_title = input('book_title: ')
+        return_at = input('return_at [yyyy-mm-dd]: ')
+        dbc.add_book(email, name, book_title, return_at)
+
+    elif len(argv) == 2 and argv[1] == 'show-books':
+        dbc = DataBaseController()
+        all_lent_books = dbc.get_all()
+        for lent_book in all_lent_books:
+            print(f"""
+                ID: {lent_book.id}, 
+                Email: {lent_book.email}, 
+                Name: {lent_book.name}, 
+                Book Title: {lent_book.book_title}, 
+                Lent At: {lent_book.lent_at}
+                Return At: {lent_book.return_at}""")
+
+    elif len(argv) == 2 and argv[1] == 'clear':
+        dbc = DataBaseController()
+        dbc.clear_db()
+
+    elif len(argv) == 5 and argv[1] == 'update' and argv[2] == 'lent':
+        dbc = DataBaseController()
+        new_date = datetime.strptime(argv[3], "%Y-%m-%d").date()
+        condition_date = datetime.strptime(argv[2], "%Y-%m-%d").date()
+        dbc.update_db_lent_date(condition_date, new_date)
+
+    elif len(argv) == 5 and argv[1] == 'update' and argv[2] == 'return':
+        dbc = DataBaseController()
+        new_date = datetime.strptime(argv[3], "%Y-%m-%d").date()
+        condition_date = datetime.strptime(argv[2], "%Y-%m-%d").date()
+        dbc.update_db_return_date(condition_date, new_date)
+
+    else:
+        dbc = DataBaseController()
+        dbc.send_mail_when_event_day()
