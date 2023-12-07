4ebb1868883e89bccdda01006ef5d47f7f3e415e
Czaja <dczajka1@gmail.com>
2023-12-07 22:57:11 +0100

    added commands to command line menu

--- Moduł_3/main_v2.py
+++ Moduł_3/main_v2.py
@@ -1,10 +1,31 @@
+from datetime import datetime
+
 import click
 from database import DataBaseController
+from mailing import EmailSenderManager
 
 
 @click.group()
 def cli():
     pass
+
+
+@cli.command(help='Initialize new database in current directory.')
+def create_db():
+    dbc = DataBaseController()
+    dbc.create_db()
+    click.echo('create-db')
+
+
+@cli.command(help='Adds one book to database. When launched follow the prompts')
+def add_book():
+    dbc = DataBaseController()
+    click.echo('Provide information')
+    email = input('email: ')
+    name = input('name: ')
+    book_title = input('book_title: ')
+    return_at = input('return_at [yyyy-mm-dd]: ')
+    dbc.add_book(email, name, book_title, return_at)
 
 
 @cli.command(help='Shows all books depending on given query')
@@ -42,6 +63,42 @@ def show_books():
             Lent At: {lent_book.lent_at}
             Return At: {lent_book.return_at}"""
         )
+
+
+@cli.command(help='Clears database content')
+def clear():
+    click.echo('clear!')
+
+
+@cli.command(help='Updates record with given ID to given values as kwargs')
+@click.argument('record_id')
+@click.option('--param', '-p',
+              required=True,
+              prompt='Example kwargs: return_at=2020-02-20')
+def update_by_id(record_id, param):
+    click.echo(f'the_id: {record_id}')
+    click.echo(f'param: {param}')
+    dbc = DataBaseController()
+    dbc.update_db_by_id_with_query(record_id, param)
+
+
+@cli.command(help='Check if date to return book is today and sends reminder via email')
+def check_and_send():
+    dbc = DataBaseController()
+    today = datetime.today().date()
+    receivers = dbc.check_when_event_day(today)
+    print('receivers: ', receivers)
+    with EmailSenderManager(ssl_enable=True) as manager:
+        for receiver in receivers:
+            msg = manager.create_message(
+                receiver.name,
+                receiver.email,
+                receiver.book_title,
+                receiver.lent_date
+            )
+
+            print(receiver.lent_date)
+            manager.send_mail(receiver, msg)
 
 
 if __name__ == '__main__':
